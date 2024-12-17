3e4973dd8663b8f7aa8bb28b7da01c871230455f
Sakpal Amit <amitsakpal744@gmail.com>
2024-12-17 20:08:45 +0530

    Completed add patient method , with execption handling , one can now add patient , with their corressponding input asked , inserts into the db

--- HospitalManagementSystem/Patient.java
+++ HospitalManagementSystem/Patient.java
@@ -2,6 +2,8 @@ package HospitalManagementSystem;
 
 
 import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
 import java.util.Scanner;
 
 public class Patient {
@@ -30,6 +32,28 @@ public class Patient {
         System.out.printl("Enter Patient Gender : ");
 
         String gender = scanner.next();
+
+        // blocks //
+        try{
+            String query = "INSERT INTO patient(name,age,gender) VALUES(?,?,?)";
+            PreparedStatement preparedStatement = connection.prepareStatement(query);
+
+            preparedStatement.setString(1,name);
+            preparedStatement.setInt(2,age);
+            preparedStatement.setString(3,gender);
+
+            int affectedRows = preparedStatement.executeUpdate();
+
+            if(affectedRows > 0){
+                System.out.println("Patient Added Successfully !");
+            }
+            else{
+                System.out.println("Failed to add Patient !");
+            }
+
+        }catch(SQLException e){
+            e.printStackTrace();
+        }
 
     }
 
