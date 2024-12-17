1a48cb56590f4965b56b95d83ef3b53af1a4458c
Sakpal Amit <amitsakpal744@gmail.com>
2024-12-17 19:45:52 +0530

    first commit , created connection and scanner private ables

---
+++ HospitalManagementSystem/Patient.java
@@ -0,0 +1,18 @@
+package HospitalManagementSystem;
+
+import java.sql.Connection;
+import java.util.Scanner;
+
+public class Patient {
+    // will use access modifier and encap//
+
+    private Connection connection;
+    private Scanner scanner;
+
+
+
+
+
+
+
+}
